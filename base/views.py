669b91e5caac62781c4379cbff04101022107d5a
unknown <graciousfx@gmail.com>
2025-07-04 09:56:08 +0100

    Adding required templates to the projects

--- base/views.py
+++ base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def service_detail(request, id):
+    return render(request, 'service-detail.html')
